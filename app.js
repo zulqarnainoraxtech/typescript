8344746c9c18b93b1e85a3b0f1ade1b57df16915
zulqarnain <zulqarnain.oraxtech@gmail.com>
2025-12-05 14:43:33 +0500

    initial commit exploring arrays and functions

---
+++ app.js
@@ -0,0 +1,15 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+let student = {
+    Name: "Zulqarnain",
+    Age: 23,
+    Phone: 976789
+};
+let studentsList = [
+    { Name: "Ahmad", Age: 22, Phone: 54342345 },
+    { Name: "Ali", Age: 20, Phone: 543242345 },
+    { Name: "Farhan", Age: 25, Phone: 32433345 },
+];
+studentsList.push(student);
+console.log(studentsList);
+//# sourceMappingURL=app.js.map
---
+++ app.ts
@@ -0,0 +1,59 @@
+// enum LanguageKnown {
+//     English = "English",
+//     German = "German",
+//     French = "French"
+// }
+
+// let student = {
+//     Name: "Zulqarnain",
+//     Age: 23,
+//     Phone: 976789,
+//     Language: LanguageKnown.English
+// }
+
+
+// let studentsList = [
+//     { Name: "Ahmad", Age: 22, Phone: 54342345, Language: LanguageKnown.German },
+//     { Name: "Ali", Age: 20, Phone: 543242345, Language: LanguageKnown.French },
+//     { Name: "Farhan", Age: 25, Phone: 32433345, Language: LanguageKnown.German },
+// ];
+
+// studentsList.push(student)
+
+// for (const element of studentsList) {
+//     console.log(
+//         "Name: " + element.Name +
+//         " Age: " + element.Age +
+//         " Language Known: " + element.Language
+//     );
+// }
+
+// function getAllStudents(students: any[]) {
+//     students.forEach(element => {
+//         console.log(
+//             "Name: " + element.Name +
+//             " Age: " + element.Age +
+//             " Language Known: " + element.Language
+//         );
+//     });
+// }
+
+// getAllStudents(studentsList)
+
+//Rest parameter
+
+// function getNums(...nums:number[]){
+//     nums.forEach(element => {
+//         console.log("Number ", element);
+//     });
+// }
+
+// getNums(1,2,3,4,5);
+
+
+//Default parameter 
+function getInfo(info:string = "Happy"){
+    console.log(info)
+}
+getInfo("sad");//sad will print
+
